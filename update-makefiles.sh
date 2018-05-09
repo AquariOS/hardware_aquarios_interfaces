4782ec0e3b22925e03f2242687acb0520a6a5c09
rc420head <dennislebhardt79@gmail.com>
2018-05-09 10:59:41 -0500

    AquariOS
    Change-Id: I3f9e008d42f00b9f30f460daa5bc16eea52e1fd3

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.slim:hardware/slim/interfaces" \
+  "vendor.aquarios:hardware/aquarios/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
